7c521460c258e479047d43567c80dc5f724d3785
Lam Gia Thuan <duckladydinh@gmail.com>
2020-04-06 23:14:07 +0200

    CM04: add tooltip for avatar in ChatMessage.

--- ui/src/components/ChatMessage.js
+++ ui/src/components/ChatMessage.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import Popover from '@material-ui/core/Popover';
 
 const useStyles = makeStyles((theme) => ({
     rightMessage: {
@@ -16,14 +17,60 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(2),
         width: `70%`,
     },
+    popover: {
+        pointerEvents: 'none',
+    },
+    paper: {
+        padding: theme.spacing(1),
+    },
 }));
 
 export const ChatMessage = ({user, message, isOwned}) => {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+
     return <Paper className={isOwned ? classes.leftMessage : classes.rightMessage}>
         <Grid container wrap="nowrap" spacing={2}>
             <Grid item>
-                <Avatar>{user[0]}</Avatar>
+                <Avatar
+                    aria-owns={open ? 'mouse-over-popover' : undefined}
+                    aria-haspopup="true"
+                    onMouseEnter={handlePopoverOpen}
+                    onMouseLeave={handlePopoverClose}
+                >
+                    {user[0]}
+                </Avatar>
+                <Popover
+                    id="mouse-over-popover"
+                    className={classes.popover}
+                    classes={{
+                        paper: classes.paper,
+                    }}
+                    open={open}
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    onClose={handlePopoverClose}
+                    disableRestoreFocus
+                >
+                    <Typography>{user}</Typography>
+                </Popover>
             </Grid>
             <Grid item xs>
                 <Typography>{message}</Typography>
