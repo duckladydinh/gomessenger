f22fcc39d8a2930ef8f904af74bef3a2a0ba4f6f
Gia Thuan Lam <duckladydinh@gmail.com>
2020-04-25 23:03:33 +0200

    CM12: final folder organization

--- proto.sh
+++ proto.sh
@@ -1,5 +1,5 @@
 protoc -Iproto proto/chat_service.proto \
-  --go_out=plugins=grpc:go/rpc \
-  --dart_out=grpc:flutter/lib/rpc
-#  --js_out=import_style=commonjs:web/src/rpc/ \
-#  --grpc-web_out=import_style=commonjs,mode=grpcwebtext:web/src/rpc/ \
+  --go_out=plugins=grpc:gms-go/rpc \
+  --dart_out=grpc:gms-flutter/lib/rpc
+#  --js_out=import_style=commonjs:gms-web/src/rpc/ \
+#  --grpc-web_out=import_style=commonjs,mode=grpcwebtext:gms-web/src/rpc/ \
